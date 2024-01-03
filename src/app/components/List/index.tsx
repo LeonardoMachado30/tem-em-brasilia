1f3117c80fded0cbc8d4574f2fdf70018a92de95
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2024-01-03 15:15:33 -0300

    CHANGE: Component card

--- src/app/components/List/index.tsx
+++ src/app/components/List/index.tsx
@@ -10,6 +10,10 @@ import { ModalDelete } from "../Modal/ModalDelete";
 import { ButtonClose } from "../Buttons/ButtonClose";
 import { ModalAdd } from "@/components/Modal/ModalAdd";
 import { FirebaseServices } from "@/app/firebase/FirebaseServices";
+import icon_instagram from "$/img/icons/instagram.png";
+import icon_facebook from "$/img/icons/facebook.png";
+import icon_linkedin from "$/img/icons/linkedin.png";
+import icon_youtube from "$/img/icons/youtube.png";
 
 function Card({ handleClose }: { handleClose: (data: any) => void }) {
   const firestore = useFirestore();
@@ -26,28 +30,70 @@ function Card({ handleClose }: { handleClose: (data: any) => void }) {
   return (
     <>
       {data.map((employers, index) => (
-        <div
+        <a
+          href="#"
           key={employers.id}
-          className="flex flex-col relative rounded-xl min-h-[160px] max-w-[300px] w-full shadow-xl"
+          className="flex flex-col relative rounded h-[322px] max-w-[450px] w-full shadow bg-white"
         >
-          <div className="w-5 self-end absolute top-4 right-4">
+          {/* <div className="w-5 self-end absolute top-4 right-4">
             <ButtonClose handleClose={() => handleClose(employers)} />
-          </div>
-
+          </div> */}
           <Image
             src={logo_dvx}
             alt="Logo dvx"
-            width={0}
-            height={0}
-            className="!w-full max-h-[200px] rounded-xl bg-cover"
+            width={460}
+            height={244}
+            style={{ maxHeight: "244px" }}
+            className="w-full rounded rounded-b-none absolute top-0 left-0"
           />
 
-          <div className="w-full text-center uppercase p-4">
-            <p>{employers.name}</p>
-            {/* <p>{email}</p>
-        <p>{adress}</p> */}
+          <div className="flex-col gap-2 w-full p-2 z-10 bg-white">
+            <div className="flex w-full gap-2">
+              <div className="max-w-[134px] w-full relative">
+                <Image
+                  src={logo_dvx}
+                  alt="Profile"
+                  width={134}
+                  height={134}
+                  className="rounded-full border-4 border-white absolute bottom-0"
+                />
+              </div>
+
+              <div className="flex flex-col">
+                <span
+                  className="text-lg font-bold overflow-hidden whitespace-nowrap w-[250px]"
+                  title={employers.name}
+                >
+                  {employers.name}
+                </span>
+                <span className="text-[#339B5B] font-semibold">
+                  Restaurante
+                </span>
+                <p className="font-semibold py-4">Q 25 C 15 Gama Leste</p>
+              </div>
+            </div>
+
+            <div className="flex w-full justify-around">
+              <button className="bg-[#2FAE1F] px-6 py-2 text-bold">
+                (61) 9 8109-5126
+              </button>
+              <div className="flex gap-3">
+                <button>
+                  <Image src={icon_instagram} alt="" width={36} height={36} />
+                </button>
+                <button>
+                  <Image src={icon_facebook} alt="" width={36} height={36} />
+                </button>
+                <button>
+                  <Image src={icon_linkedin} alt="" width={36} height={36} />
+                </button>
+                <button>
+                  <Image src={icon_youtube} alt="" width={36} height={36} />
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
+        </a>
       ))}
     </>
   );
@@ -69,11 +115,11 @@ function List() {
       )}
 
       <div className="flex flex-col gap-10 w-full">
-        <ButtonPrimary onClick={() => setModalAdd(!modalAdd)}>
+        {/* <ButtonPrimary onClick={() => setModalAdd(!modalAdd)}>
           Adicionar
-        </ButtonPrimary>
+        </ButtonPrimary> */}
 
-        <div className="relative flex flex-wrap justify-center md:justify-start gap-8 mx-auto p-4 w-full">
+        <div className="relative flex flex-wrap justify-center md:justify-start gap-2 mx-auto p-2 w-full max-w-[1420px]">
           <FirebaseServices>
             <Card
               handleClose={(data) => {
