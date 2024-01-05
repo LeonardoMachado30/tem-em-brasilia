9a5f1f1e010c35d867edbc79fa75a8f9e672b07b
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2024-01-05 12:20:26 -0300

    ADD: Metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,24 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Tem em Brasilia",
+  title: "Tem em Brasilia | Catalogo de empresas no Distrito Federal",
   description: "Catalogo de empresas no distrito federal",
+  authors: [
+    {
+      name: "DVX soluções tecnlogicas team",
+      url: "https://nextjs.org",
+    },
+  ],
+  keywords:
+    "Tem em Brasilia, Empresas, Expresas Distrito Federal, Brasilia, achei em brasilia",
+  robots: "index, follow",
+  publisher: "DVX",
+  referrer: "origin",
+  abstract: "Catalogo de empresas no distrito federal",
 };
 
 export default function RootLayout({
