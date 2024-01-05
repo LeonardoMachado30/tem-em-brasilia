d8e89f56cfb5ca0af450dc67780f0798938ac505
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2024-01-05 12:21:19 -0300

    Change: transfer component card for indivual component

--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -1,5 +1,109 @@
-function Card() {
-  return <div></div>;
+import Image, { StaticImageData } from "next/image";
+import { useFirestoreCollectionData, useFirestore } from "reactfire";
+import { collection, orderBy, query } from "firebase/firestore";
+import { ButtonPrimary } from "../Buttons/ButtonPrimary";
+import logo_dvx from "$/img/logo-dvx.png";
+import { ButtonClose } from "../Buttons/ButtonClose";
+import icon_instagram from "$/img/icons/instagram.png";
+import icon_facebook from "$/img/icons/facebook.png";
+import icon_linkedin from "$/img/icons/linkedin.png";
+import icon_youtube from "$/img/icons/youtube.png";
+import icon_whatsapp from "$/img/icons/whatsapp.png";
+import { BuildingOffice2Icon, MapPinIcon } from "@heroicons/react/24/outline";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+type SocialMediasProps = {
+  image: StaticImageData;
+  alt: string;
+  link: string;
+};
+
+function SocialMedias() {
+  const imageArr: SocialMediasProps[] = [
+    { image: icon_whatsapp, alt: "icon whatsapp", link: "" },
+    { image: icon_instagram, alt: "icon instagram", link: "" },
+    { image: icon_facebook, alt: "icon facebook", link: "" },
+  ];
+  return (
+    <div className="flex gap-2 w-full items-center item-hidden">
+      {imageArr.map(({ image, alt }: SocialMediasProps, index) => {
+        return (
+          <a key={index}>
+            <Image src={image} alt={alt} width={36} height={36} />
+          </a>
+        );
+      })}
+    </div>
+  );
 }
 
+function Card() {
+  const firestore = useFirestore();
+  const employersCollection = collection(firestore, "employers");
+  const employersQuery = query(employersCollection, orderBy("name", "desc"));
+  const { status, data: data } = useFirestoreCollectionData(employersQuery, {
+    idField: "id",
+  });
+
+  return (
+    <>
+      {status === "loading" ? (
+        <Skeleton count={2} />
+      ) : (
+        data.map((employers, index) => (
+          <a
+            href="#"
+            key={employers.id}
+            className="flex flex-col relative rounded-md max-w-[420px] w-full shadow bg-white open-info"
+          >
+            <div className="shadow-gray-500 shadow-inner rounded-b-none rounded-t-md rounded-tr-md">
+              <Image
+                src={logo_dvx}
+                alt="Logo dvx"
+                width={460}
+                height={160}
+                style={{ maxHeight: "160px" }}
+                className="rounded-b-none rounded-t-md rounded-tr-md"
+              />
+            </div>
+            <div className="relative flex flex-col px-4 py-2 container-animation bg-white">
+              <div className="flex gap-2">
+                <Image
+                  src={logo_dvx}
+                  alt={`Foto de perfil da empresa ${employers.name}`}
+                  width={100}
+                  height={100}
+                  className="rounded-full border-4 border-white -mt-8"
+                />
+                <div className="flex flex-col">
+                  <p
+                    className="text-lg font-bold ellipse"
+                    title={employers.name}
+                  >
+                    {employers.name}
+                  </p>
+                  <span className="flex items-center gap-1">
+                    <BuildingOffice2Icon className="h-5 w-5 text-[#339B5B]" />
+                    <p className="text-[#339B5B] font-semibold text-sm">
+                      Restaurante
+                    </p>
+                  </span>
+
+                  <span className="flex items-center gap-1">
+                    <MapPinIcon className="h-5 w-5 text-[#339B5B]" />
+                    <p className="text-[#339B5B] font-semibold text-sm">
+                      Q 25 C 15 Gama Leste
+                    </p>
+                  </span>
+                </div>
+              </div>
+              <SocialMedias />
+            </div>
+          </a>
+        ))
+      )}
+    </>
+  );
+}
 export { Card };
--- src/app/components/List/index.tsx
+++ src/app/components/List/index.tsx
@@ -1,107 +1,13 @@
 "use client";
-import Image from "next/image";
-import { useFirestoreCollectionData } from "reactfire";
-import { collection, orderBy, query } from "firebase/firestore";
-import { useFirestore } from "reactfire";
 import { useState } from "react";
-import { ButtonPrimary } from "../Buttons/ButtonPrimary";
-import logo_dvx from "$/img/logo-dvx.png";
 import { ModalDelete } from "../Modal/ModalDelete";
-import { ButtonClose } from "../Buttons/ButtonClose";
 import { ModalAdd } from "@/components/Modal/ModalAdd";
 import { FirebaseServices } from "@/app/firebase/FirebaseServices";
-import icon_instagram from "$/img/icons/instagram.png";
-import icon_facebook from "$/img/icons/facebook.png";
-import icon_linkedin from "$/img/icons/linkedin.png";
-import icon_youtube from "$/img/icons/youtube.png";
-
-function Card({ handleClose }: { handleClose: (data: any) => void }) {
-  const firestore = useFirestore();
-  const employersCollection = collection(firestore, "employers");
-  const employersQuery = query(employersCollection, orderBy("name", "desc"));
-  const { status, data: data } = useFirestoreCollectionData(employersQuery, {
-    idField: "id",
-  });
-
-  if (status === "loading") {
-    return <p>carregando...</p>;
-  }
-
-  return (
-    <>
-      {data.map((employers, index) => (
-        <a
-          href="#"
-          key={employers.id}
-          className="flex flex-col relative rounded h-[322px] max-w-[450px] w-full shadow bg-white"
-        >
-          {/* <div className="w-5 self-end absolute top-4 right-4">
-            <ButtonClose handleClose={() => handleClose(employers)} />
-          </div> */}
-          <Image
-            src={logo_dvx}
-            alt="Logo dvx"
-            width={460}
-            height={244}
-            style={{ maxHeight: "244px" }}
-            className="w-full rounded rounded-b-none absolute top-0 left-0"
-          />
-
-          <div className="flex-col gap-2 w-full p-2 z-10 bg-white">
-            <div className="flex w-full gap-2">
-              <div className="max-w-[134px] w-full relative">
-                <Image
-                  src={logo_dvx}
-                  alt="Profile"
-                  width={134}
-                  height={134}
-                  className="rounded-full border-4 border-white absolute bottom-0"
-                />
-              </div>
-
-              <div className="flex flex-col">
-                <span
-                  className="text-lg font-bold overflow-hidden whitespace-nowrap w-[250px]"
-                  title={employers.name}
-                >
-                  {employers.name}
-                </span>
-                <span className="text-[#339B5B] font-semibold">
-                  Restaurante
-                </span>
-                <p className="font-semibold py-4">Q 25 C 15 Gama Leste</p>
-              </div>
-            </div>
-
-            <div className="flex w-full justify-around">
-              <button className="bg-[#2FAE1F] px-6 py-2 text-bold">
-                (61) 9 8109-5126
-              </button>
-              <div className="flex gap-3">
-                <button>
-                  <Image src={icon_instagram} alt="" width={36} height={36} />
-                </button>
-                <button>
-                  <Image src={icon_facebook} alt="" width={36} height={36} />
-                </button>
-                <button>
-                  <Image src={icon_linkedin} alt="" width={36} height={36} />
-                </button>
-                <button>
-                  <Image src={icon_youtube} alt="" width={36} height={36} />
-                </button>
-              </div>
-            </div>
-          </div>
-        </a>
-      ))}
-    </>
-  );
-}
+import { Card } from "../Card";
 
 function List() {
   const [modalAdd, setModalAdd] = useState<boolean>(false);
-  const [modalDelete, setModalDelete] = useState(false);
+  const [modalDelete, setModalDelete] = useState<boolean>(false);
   const [data, setData] = useState({});
 
   return (
@@ -114,22 +20,25 @@ function List() {
         />
       )}
 
-      <div className="flex flex-col gap-10 w-full">
+      <section className="flex flex-col justify-center items-center gap-2 w-full mx-auto max-w-[1420px] my-24 ">
         {/* <ButtonPrimary onClick={() => setModalAdd(!modalAdd)}>
           Adicionar
         </ButtonPrimary> */}
-
-        <div className="relative flex flex-wrap justify-center md:justify-start gap-2 mx-auto p-2 w-full max-w-[1420px]">
+        <h2 className="text-[#75AE8B]">
+          Maior catalógo de empresas do Distrito Federal.
+        </h2>
+        <h3 className="text-[#75AE8B]">
+          Anuncie sua empresa <strong>aqui</strong>.
+        </h3>
+        <h1 className="text-4xl font-bold text-[#006728] my-6">
+          Empresas recentes
+        </h1>
+        <div className="relative flex flex-wrap justify-center items-center md:justify-start gap-4 mx-auto p-1 w-full">
           <FirebaseServices>
-            <Card
-              handleClose={(data) => {
-                setData(data);
-                setModalDelete(!modalDelete);
-              }}
-            />
+            <Card />
           </FirebaseServices>
         </div>
-      </div>
+      </section>
     </>
   );
 }
