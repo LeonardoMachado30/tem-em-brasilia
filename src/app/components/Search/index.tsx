f8fd908c70095bf69c7a6a4ba4d31f0c75d9bf8f
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2024-01-05 12:21:41 -0300

    Change; add background image

--- src/app/components/Search/index.tsx
+++ src/app/components/Search/index.tsx
@@ -7,9 +7,9 @@ import search from "$/img/search.svg";
 import Image from "next/image";
 import firebase from "firebase/compat/app";
 import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
-
+import bg_brasilia from "$/img/bg-brasilia.png";
 function Search() {
-  const [search, setSearch] = useState<string>(""); 
+  const [search, setSearch] = useState<string>("");
   // const alert = useContext(AlertContext);
   // const firestore = useFirestore();
 
@@ -29,28 +29,31 @@ function Search() {
   }
 
   return (
-    <div className="flex items-center w-full max-w-[300px] mx-auto">
-
-      <div className="relative w-full">
-        <input
-          onChange={(e) => {
-            setSearch(e.target.value);
-          }}
-          type="text"
-          id="simple-search"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-2 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Search branch name..."
-          required
-        />
+    <section
+      className=" w-full h-[836px] bg-norepeat animation-bg"
+      style={{ backgroundImage: `url(${bg_brasilia.src})` }}
+    >
+      <div className="max-w-[728px] w-full mx-auto h-full flex justify-center items-center">
+        <div className="relative w-full h-[48px]">
+          <input
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+            type="text"
+            className="p-2.5 ms-2 text-sm font-medium text-[#339B5B] bg-white rounded-l rounded-bl border-[#339B5B] border-2 focus:outline-none w-full placeholder:text-[#339B5B] h-[48px]"
+            placeholder="Search branch name..."
+            required
+          />
+        </div>
+        <button
+          onClick={SearchFirebase}
+          type="submit"
+          className="p-2.5 ms-2 text-sm font-medium text-white bg-[#339B5B]  rounded-r rounded-br rounded-bl-none border border-[#339B5B] hover:bg-[#339B5B] focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-[#339B5B] dark:hover:bg-[#339B5B] dark:focus:bg-[#339B5B] h-[48px]"
+        >
+          <MagnifyingGlassIcon className="h-6 w-6 text-white  " />
+        </button>
       </div>
-      <button
-        onClick={SearchFirebase}
-        type="submit"
-        className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        <MagnifyingGlassIcon className="h-6 w-6 text-white  " />
-      </button>
-    </div>
+    </section>
   );
 }
 
