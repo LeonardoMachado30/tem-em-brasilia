d2a9a2128ec5b5d03e941a71c04bbee0c8840627
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2024-01-08 17:53:39 -0300

    ADD ProviderSearch in the page view home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
-import { List } from "@/components/List";
 import { Search } from "@/components/Search";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import { SearchProvider } from "./lib/context/searchContext";
+import { ListAllCompanies } from "./components/Card";
+import { FirebaseServices } from "./firebase/FirebaseServices";
 
 export default function Home() {
   return (
     <main>
       <Header />
-
-      <Search />
-
-      <List />
+      <SearchProvider>
+        <Search />
+        <ListAllCompanies />
+      </SearchProvider>
 
       <Footer />
     </main>
