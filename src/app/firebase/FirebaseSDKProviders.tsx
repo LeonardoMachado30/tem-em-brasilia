9d636f7515c8a8f009d6d90514de4e298141e2d6
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2024-01-17 09:08:09 -0300

    CHANGE: Add GoogleOAuth Provider

--- src/app/firebase/FirebaseSDKProviders.tsx
+++ src/app/firebase/FirebaseSDKProviders.tsx
@@ -9,6 +9,7 @@ import {
 import { getAuth } from "firebase/auth"; // Firebase v9+
 import { getDatabase } from "firebase/database"; // Firebase v9+
 import { IChildren } from "@/model/EmployerModel";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const FirebaseSDKProviders = ({ children }: IChildren) => {
   const app = useFirebaseApp();
@@ -19,7 +20,9 @@ const FirebaseSDKProviders = ({ children }: IChildren) => {
   return (
     <FirestoreProvider sdk={firestoreInstance}>
       <AuthProvider sdk={auth}>
-        <DatabaseProvider sdk={database}>{children}</DatabaseProvider>
+        <GoogleOAuthProvider clientId="260974977064-fcs596908nd2cjv1k839tpjb5rbdj516.apps.googleusercontent.com">
+          <DatabaseProvider sdk={database}>{children}</DatabaseProvider>
+        </GoogleOAuthProvider>
       </AuthProvider>
     </FirestoreProvider>
   );
