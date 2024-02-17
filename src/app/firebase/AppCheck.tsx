f635b95e59b12ab5caca6fc9a7edce8c2f5084c5
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2024-02-17 22:26:42 +0000

    [CHANGE]: separete method get storage image

--- src/app/firebase/AppCheck.tsx
+++ src/app/firebase/AppCheck.tsx
@@ -1,16 +1,8 @@
-"use client";
-
 import { ReCaptchaV3Provider, initializeAppCheck } from "firebase/app-check";
 
 import { firebaseApp } from "./firebaseInitApp";
-if (!location.host.includes("temembrasilia")) {
-  self.FIREBASE_APPCHECK_DEBUG_TOKEN =
-    process.env.APP_CHECK_DEBUG_TOKEN_FROM_CI;
-}
 
-const appCheck = initializeAppCheck(firebaseApp, {
+export default initializeAppCheck(firebaseApp, {
   provider: new ReCaptchaV3Provider("6Lc5BWUpAAAAAMXSow9T8oPwxcRQfjoONMXlUbG5"),
   isTokenAutoRefreshEnabled: true,
 });
-
-export default appCheck;
